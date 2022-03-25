c10e810b4f12fbac758c489c1300de2702d9e7b1
mario31yuma <mario31yuma@uma.es>
2022-03-25 10:04:06 +0100

    Creación de la clase DepositadaEn, implementadas sus variables y los metodos getters, setters, equals, hashcode y toString.

--- fintech/fintech-jpa/src/main/java/ma3s/fintech/DepositadaEn.java
+++ fintech/fintech-jpa/src/main/java/ma3s/fintech/DepositadaEn.java
@@ -1,4 +1,44 @@
 package ma3s.fintech;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class DepositadaEn {
+    @Id
+    private Double saldo;
+
+
+    public DepositadaEn(){
+
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DepositadaEn that = (DepositadaEn) o;
+        return saldo.equals(that.saldo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(saldo);
+    }
+
+    @Override
+    public String toString() {
+        return "DepositadaEn{" +
+                "saldo=" + saldo +
+                '}';
+    }
 }
