f814ddda627c925056b22bc3ff015da068a5d5dc
salvaromanes <salvaromanes@uma.es>
2022-04-13 18:14:28 +0200

    Interfaz GestionCierreCuenta creada

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/GestionCierreCuenta.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/GestionCierreCuenta.java
@@ -1,4 +1,8 @@
 package ma3s.fintech;
 
+import ma3s.fintech.excepciones.CuentaNoExistenteException;
+import ma3s.fintech.excepciones.CuentaNoVacia;
+
 public interface GestionCierreCuenta {
+    public void cerrarCuenta(String iban) throws CuentaNoExistenteException, CuentaNoVacia;
 }
