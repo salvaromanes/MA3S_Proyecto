2e5e1ac09bd5d2d64c6bfd312c11508f7a2872f6
almubalmu <almubalmu@uma.es>
2022-04-15 13:40:12 +0200

    10 mejorado

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/GestionAccesoAplicacion.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/GestionAccesoAplicacion.java
@@ -1,9 +1,11 @@
 package ma3s.fintech;
 
+import ma3s.fintech.excepciones.ContraseñaIncorrectaException;
 import ma3s.fintech.excepciones.PersonaNoExisteException;
+import ma3s.fintech.excepciones.UsuarioIncorrectoException;
 
 public interface GestionAccesoAplicacion {
-    void accederAplicacion(Long idCliente) throws PersonaNoExisteException;
+    void accederAplicacion(Long idCliente, String usuario, String contrasena) throws PersonaNoExisteException, UsuarioIncorrectoException, ContraseñaIncorrectaException;
     boolean isClientePersonaJuridica(Long id, String tipoCliente) throws PersonaNoExisteException;
     boolean isPersonaAutorizada(Long idPA, Long idCliente) throws PersonaNoExisteException;
 }
