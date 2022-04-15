ccfc1a1afae52f9f9d7a6c0e663b01a3a132810f
mario31yuma <mario31yuma@uma.es>
2022-04-15 14:12:57 +0200

    Versión 1.0 de Requisito 14 (GestionTransferencia.java y Transferencias.java)

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/GestionTransferencia.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/GestionTransferencia.java
@@ -2,10 +2,11 @@ package ma3s.fintech;
 
 import ma3s.fintech.excepciones.CuentaExistenteException;
 import ma3s.fintech.excepciones.ErrorOrigenTransaccionException;
+import ma3s.fintech.excepciones.PersonaNoExisteException;
 
 public interface GestionTransferencia {
 
-    public void transferenciaClienteAutorizado(Long id, String IBAN, double cantidad) throws CuentaExistenteException,
+    public void transferenciaClienteAutorizado(Long id, String IBAN_origen, String IBAN_destino, double cantidad) throws PersonaNoExisteException, CuentaExistenteException,
             ErrorOrigenTransaccionException;
 
 }
---
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/Transferencias.java
@@ -0,0 +1,39 @@
+package ma3s.fintech;
+
+import ma3s.fintech.excepciones.CuentaExistenteException;
+import ma3s.fintech.excepciones.ErrorOrigenTransaccionException;
+import ma3s.fintech.excepciones.PersonaNoExisteException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class Transferencias implements GestionTransferencia{
+
+    @PersistenceContext(name="fintech")
+    private EntityManager em;
+
+    @Override
+    public void transferenciaClienteAutorizado(Long id, String IBAN_origen, String IBAN_destino, double cantidad) throws PersonaNoExisteException, CuentaExistenteException, ErrorOrigenTransaccionException {
+        // Comprobar que el id es una cuenta o persona física o persona
+        Empresa empresa = em.find(Empresa.class, id);
+        PAutorizada pAutorizada = em.find(PAutorizada.class, id);
+
+        if (empresa == null && pAutorizada == null) {
+            throw new PersonaNoExisteException("No existe el cliente con id: " + id + ".");
+        }
+
+        // Comprobar que la PAutorizada es sobre persona física
+
+
+        // Comprobar que estamos sobre una empresa
+        if(empresa != null){
+            if(!empresa.getCuentasFintech().contains(IBAN_origen)){
+                throw new ErrorOrigenTransaccionException("El cliente no tiene acceso a la cuenta: " + IBAN_origen + ".");
+            }
+
+
+
+        }
+
+    }
+}
