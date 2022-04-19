be3e7db650ce07436bef3a4a17e51a8e2afd4426
salvaromanes <salvaromanes@uma.es>
2022-04-19 21:03:59 +0200

    Cambios post tutoria

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/AperturaCuenta.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/AperturaCuenta.java
@@ -23,9 +23,6 @@ public class AperturaCuenta implements GestionAperturaCuenta{
         }
     }
 
-    // falta hacer el persist en ambas
-
-
     public void abrirCuentaPooled(String iban, String swift) throws CuentaExistenteException{
         Cuenta cuenta = em.find(Cuenta.class, iban);
 
