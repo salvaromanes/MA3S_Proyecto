c2bd894655331e72f2dced69e88d54b961b15d6b
mario31yuma <mario31yuma@uma.es>
2022-04-22 22:40:33 +0200

    Version 1.2 de Requisito 14 (GestionTransferencia.java y TransaccionYaExistente.java y Transferencias.java) Creacion de SaldoNoSuficiente.java

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/GestionTransferencia.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/GestionTransferencia.java
@@ -1,13 +1,15 @@
 package ma3s.fintech;
 
-import ma3s.fintech.excepciones.CuentaExistenteException;
-import ma3s.fintech.excepciones.ErrorOrigenTransaccionException;
-import ma3s.fintech.excepciones.PersonaNoExisteException;
-import ma3s.fintech.excepciones.TransaccionYaExistente;
+import ma3s.fintech.excepciones.*;
+
+import java.util.Date;
 
 public interface GestionTransferencia {
 
-    public void transferenciaClienteAutorizado(Long id, String idTransaccion, String IBAN_origen, String IBAN_destino, double cantidad) throws PersonaNoExisteException, CuentaExistenteException,
-            ErrorOrigenTransaccionException, TransaccionYaExistente;
+    public void transferenciaCliente(Long id, String idTransaccion, String IBAN_origen, String IBAN_destino, double cantidad, String divisaOrigen, Date fechaInstruccion)
+    throws PersonaNoExisteException, CuentaExistenteException, ErrorOrigenTransaccionException, TransaccionYaExistente, SaldoNoSuficiente;
+
+    public void transferenciaAutorizado(Long id, String idTransaccion, String IBAN_origen, String IBAN_destino, double cantidad, String divisaOrigen, Long idEmpresa, Date fechaInstruccion)
+    throws PersonaNoExisteException, CuentaExistenteException, ErrorOrigenTransaccionException, TransaccionYaExistente, SaldoNoSuficiente;
 
 }
