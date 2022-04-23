d1fc2e486d1f6ad7a12003034f94a5db898f75cf
almubalmu <almubalmu@uma.es>
2022-04-23 18:58:30 +0200

    RF12 terminado

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/GenerarCSV.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/GenerarCSV.java
@@ -1,12 +1,19 @@
 package ma3s.fintech;
 
 import ma3s.fintech.excepciones.PersonaNoExisteException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.io.*;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -16,7 +23,7 @@ public class GenerarCSV implements GestionGenerarCSV{
 
 
     @Override
-    public void generarCSV(String usuario, String tipoInforme, Date ultimoReporte) throws PersonaNoExisteException {
+    public void generarCSV(String usuario, String tipoInforme, Date ultimoReporte) throws PersonaNoExisteException, IOException {
         Usuario user = em.find(Usuario.class, usuario);
         Query query = em.createQuery("select c from Cliente c");
         List<Cliente> listaClientes = query.getResultList();
@@ -25,44 +32,138 @@ public class GenerarCSV implements GestionGenerarCSV{
             throw new PersonaNoExisteException("El usuario " + usuario + " no existe");
         }
 
-        if (tipoInforme.equals("Inicial")){
-            reporteInicial(listaClientes);
-        }else{ // el informe es periodico
-            ReportesPeriodicos();
+        CSVPrinter csv = null;
+        if (tipoInforme.equals("Inicial") && listaClientes != null){
+            csv = reporteInicial(listaClientes);
+        }else if (tipoInforme.equals("Periodico") && listaClientes != null){
+            csv = reportesPeriodicos(listaClientes);
         }
+
+        // pasamos el csv a un string
+        String stringCSV = csvString(csv);
     }
 
-    private void reporteInicial(List<Cliente> lista) {
-        CSVPrinter csvCabecera = new CSVPrinter(stringWriter,
+    private CSVPrinter reporteInicial(List<Cliente> lista) throws IOException {
+        final StringWriter stringWriter = new StringWriter();
+        CSVPrinter csv = new CSVPrinter(stringWriter,
                 CSVFormat.DEFAULT.withHeader("Iban", "Apellidos", "Nombre", "Calle", "Ciudad",
                         "Codigo Postal", "Pais", "Numero de Identificacion", "Fecha Nacimiento"));
 
         for (Cliente c : lista) {
             List<Fintech> cuentasCliente = c.getCuentasFintech();
             for (Fintech f : cuentasCliente) {
-                Date fechaActual = Date.from(Instant.from(LocalDateTime.now()));
-                if(f.getFechaApertura().compareTo(fechaActual) == 0){
+                Calendar fechaActual = Calendar.getInstance();
+                fechaActual.add(Calendar.YEAR, -5);
+                Date fechaHace5 = fechaActual.getTime();
+                if(f.getFechaApertura().compareTo(fechaHace5) == 0){
                     PAutorizada pa = c.getUser().getpAutorizada();
-                    csvCabecera.printRecord(
-                            f.getIban(),
-                            pa.getApellidos(),
-                            pa.getNombre(),
-                            c.getDireccion(),
-                            c.getCiudad(),
-                            c.getCodigopostal(),
-                            c.getPais(),
-                            c.getIdentificacion(),
-                            pa.getFechaNacimiento()
+                    csv.printRecord(
+                            // si algun valor es nulo debe guardarse como "noexistente"
+                            comprobarNoNulo(f.getIban()),
+                            comprobarNoNulo(pa.getApellidos()),
+                            comprobarNoNulo(pa.getNombre()),
+                            comprobarNoNulo(c.getDireccion()),
+                            comprobarNoNulo(c.getCiudad()),
+                            comprobarNoNulo(c.getCodigopostal()),
+                            comprobarNoNulo(c.getPais()),
+                            comprobarNoNulo(c.getIdentificacion()),
+                            comprobarNoNuloFecha(pa.getFechaNacimiento())
                     );
-
                 }
             }
         }
+        return csv;
     }
 
-    private void ReportesPeriodicos() {
+    private CSVPrinter reportesPeriodicos(List<Cliente> lista) throws IOException {
+        final StringWriter stringWriter = new StringWriter();
+        CSVPrinter csv = new CSVPrinter(stringWriter,
+                CSVFormat.DEFAULT.withHeader("Iban", "Apellidos", "Nombre", "Calle", "Ciudad",
+                        "Codigo Postal", "Pais", "Numero de Identificacion", "Fecha Nacimiento"));
+
+        for (Cliente c : lista) {
+            List<Fintech> cuentasCliente = c.getCuentasFintech();
+            for (Fintech f : cuentasCliente) {
+                Calendar fechaActual = Calendar.getInstance();
+                fechaActual.add(Calendar.YEAR, -5);
+                Date fechaHace5 = fechaActual.getTime();
+                if(f.getFechaApertura().compareTo(fechaHace5) == 0 &&
+                        f.getFechaCierre().compareTo(fechaActual.getTime())<0){
+                    PAutorizada pa = c.getUser().getpAutorizada();
+                    csv.printRecord(
+                            // si algun valor es nulo debe guardarse como "noexistente"
+                            comprobarNoNulo(f.getIban()),
+                            comprobarNoNulo(pa.getApellidos()),
+                            comprobarNoNulo(pa.getNombre()),
+                            comprobarNoNulo(c.getDireccion()),
+                            comprobarNoNulo(c.getCiudad()),
+                            comprobarNoNulo(c.getCodigopostal()),
+                            comprobarNoNulo(c.getPais()),
+                            comprobarNoNulo(c.getIdentificacion()),
+                            comprobarNoNuloFecha(pa.getFechaNacimiento())
+                    );
+                }
+            }
+        }
+        return csv;
     }
 
+    private Object comprobarNoNulo(String dato) {
+        if(dato.isEmpty()){
+            dato = "noexistente";
+        }
+        return dato;
+    }
+
+    private Object comprobarNoNuloFecha(Date fecha) {
+        Object fechaNula;
+        if(fecha.toString().isEmpty() || fecha.toString() == null){
+            fechaNula = "noexistente";
+        }else{
+            fechaNula = fecha;
+        }
+        return fechaNula;
+    }
+
+    private String csvString(CSVPrinter csv){
+        String salida = null;
+        String SEPARADOR = ",";
+        BufferedReader bufferLectura = null;
+
+        try {
+            // Abrir el .csv en buffer de lectura
+            bufferLectura = new BufferedReader(new FileReader(String.valueOf(csv)));
+
+            // Leer una linea del archivo
+            String linea = bufferLectura.readLine();
+
+            while (linea != null) {
+                // Sepapar la linea leída con ","
+                String[] campos = linea.split(SEPARADOR);
+                salida += Arrays.toString(campos);
+                // Volver a leer otra línea del fichero
+                linea = bufferLectura.readLine();
+            }
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        finally {
+            // Cierro el buffer de lectura
+            if (bufferLectura != null) {
+                try {
+                    bufferLectura.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return salida;
+    }
 
     // ¿el usuario es administrativo? sirve para poder gestionar las bajas
     @Override
--- fintech/fintech-ejb/src/main/java/ma3s/fintech/GestionGenerarCSV.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/GestionGenerarCSV.java
@@ -4,9 +4,10 @@ import ma3s.fintech.excepciones.AccesoException;
 import ma3s.fintech.excepciones.CuentaExistenteException;
 import ma3s.fintech.excepciones.PersonaNoExisteException;
 
+import java.io.IOException;
 import java.util.Date;
 
 public interface GestionGenerarCSV {
-    void generarCSV(String usuario, String tipoInforme, Date ultimoReporte) throws PersonaNoExisteException;
+    void generarCSV(String usuario, String tipoInforme, Date ultimoReporte) throws PersonaNoExisteException, IOException;
     boolean isAdministrativo(String usuario) throws PersonaNoExisteException;
 }
