432ab5aed534e6c39a717034324fe32117cb971f
Álvaro Salas <0619883649@uma.es>
2022-04-23 20:16:36 +0200

    RF-11

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/GeneracionInfHolanda.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/GeneracionInfHolanda.java
@@ -14,28 +14,44 @@ public class GeneracionInfHolanda implements GestionInfHolanda {
     private EntityManager em;
 
     @Override
-    public void CuentasApi(Cuenta cuenta) throws CuentaNoExistenteException {
+    public String CuentasApi(Cuenta cuenta) throws CuentaNoExistenteException {
         Cuenta cuenta_Aux = em.find(Cuenta.class, cuenta);
         if(!cuenta_Aux.equals(cuenta)){
             throw new CuentaNoExistenteException("La cuenta : " + cuenta_Aux.getIban() +  " no se encuentra" );
         }
+        return ;
     }
 
     @Override
-    public void ClienteApi(Cliente cliente) throws ClienteNoExisteException {
+    public String ClienteApi(Cliente cliente) throws ClienteNoExisteException {
          Cliente cliente1 = em.find(Cliente.class, cliente);
          if(!cliente1.equals(cliente)){
              throw  new ClienteNoExisteException("El cliente : " + cliente1.getId() + " no existe");
          }
+        String aux = "{\n \"searchParametres\":{" +
+                 "\n \"quesionType\": \"Customer,\"" +
+                 "\"startPeriod\":"+ cliente.getFechaAlta() +",\n" +
+                 "\"endPeriod\":"+ cliente.getFechaBaja() +",\n"+
+            "\"name\"{:"+ cliente.getUser() +"},\n" +
+                "\"address\"{:"+
+                "\"Number\":"+ cliente.get +",\n" +
+                cliente.getFechaAlta() +",\n"
+                ;
+
+         return  aux;
+
     }
 
     @Override
-    public void PAutorApi(PAutorizada autorizada) throws NoEsPAutorizadaException {
+    public String PAutorApi(PAutorizada autorizada) throws NoEsPAutorizadaException {
         PAutorizada autorizada1 = em.find(PAutorizada.class,autorizada);
         if(!autorizada1.equals(autorizada)){
             throw new NoEsPAutorizadaException("La persona  "+  autorizada1.getId() + " no es autorizada" );
         }
     }
+
+
+
 
 
 }
