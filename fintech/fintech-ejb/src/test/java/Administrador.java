d501d3d7ef4937acae8a95ddf4533be5c69dfb97
Álvaro Salas <0619883649@uma.es>
2022-04-26 20:40:00 +0200

    Test Requisito func 1 TERMINADO

--- fintech/fintech-ejb/src/test/java/Administrador.java
+++ fintech/fintech-ejb/src/test/java/Administrador.java
@@ -12,18 +12,22 @@ public class Administrador {
     private static final String APERTURA_CUENTA = "java:global/classes/AperturaCuenta";
     private static final String CIERRE_CUENTA = "java:global/classes/CierreCuenta";
     private static final String AccesoPersonal = "java:global/classes/AccesoPersonal";
+    private static final String AnadirAutorizado = "java:global/classes/AnadirAutorizados";
 
 
     private GestionAperturaCuenta gestionAperturaCuenta;
     private GestionCierreCuenta gestionCierreCuenta;
     private GestionAccesoPersonal gestionAccesoPersonal;
+    private GestionAnadirAutorizados gestionAnadirAutorizados;
+
+
 
     @Before
     public void setup() throws NamingException {
         gestionAperturaCuenta = (GestionAperturaCuenta) SuiteTest.ctx.lookup(APERTURA_CUENTA);
         gestionCierreCuenta = (GestionCierreCuenta) SuiteTest.ctx.lookup(CIERRE_CUENTA);
         gestionAccesoPersonal = (GestionAccesoPersonal) SuiteTest.ctx.lookup(AccesoPersonal);
-
+        gestionAnadirAutorizados = (GestionAnadirAutorizados) SuiteTest.ctx.lookup(AnadirAutorizado);
 
         BaseDatos.inicializaBaseDatos(UNIDAD_PERSITENCIA_PRUEBAS);
     }
@@ -107,6 +111,85 @@ public class Administrador {
     @Test
 
     public void testAccesoPersonalAdmNoEncontrado(){
+        final String NOMB = "alv";
+        final String CONT = "123";
+        Usuario user1 = new Usuario();
+        user1.setUser(NOMB);
+        user1.setContrasena(CONT);
+
+        try {
+            gestionAccesoPersonal.obtenerPersonal(user1.getUser(), user1.getContrasena());
+            fail("Debe lanzar una excepcion");
+        }catch (UsuarioNoEncontradoException e) {
+            //ok
+        }
+        catch (UsuarioIncorrectoException e){
+            fail("Debe lanzar una excepcion de UsuarioNoEncontrado");
+        }
+        catch (ContraseñaIncorrectaException e){
+            fail("Debe lanzar una excepcion de UsuarioNoEncontrado");
+        }
+    }
+
+
+    @Requisitos({"RF1"})
+    @Test
+
+    public void testAccesoPersonalAdmNoValido(){
+        final String NOMB = "alv";
+        final String CONT = "123";
+        Usuario user1 = new Usuario();
+        user1.setUser(NOMB);
+        user1.setContrasena(CONT);
+
+        try {
+            gestionAccesoPersonal.obtenerPersonal(user1.getUser(), user1.getContrasena());
+            fail("Debe lanzar una excepcion");
+        }catch (UsuarioNoEncontradoException e) {
+            fail("Debe lanzar una excepcion de UsuarioIncorrecto");
+        }
+        catch (UsuarioIncorrectoException e){
+            //ok
+        }
+        catch (ContraseñaIncorrectaException e){
+            fail("Debe lanzar una excepcion de UsuarioIncorrecto");
+        }
+    }
+
+
+
+
+    @Requisitos({"RF1"})
+    @Test
+
+    public void testAccesoPersonalAdmContNoValida(){
+        final String NOMB = "alv";
+        final String CONT = "123";
+        Usuario user1 = new Usuario();
+        user1.setUser(NOMB);
+        user1.setContrasena(CONT);
+
+        try {
+            gestionAccesoPersonal.obtenerPersonal(user1.getUser(), user1.getContrasena());
+            fail("Debe lanzar una excepcion");
+        }catch (UsuarioNoEncontradoException e) {
+            fail("Debe lanzar una excepcion de ContasenaIncorrecta");
+        }
+        catch (UsuarioIncorrectoException e){
+            fail("Debe lanzar una excepcion de ContasenaIncorrecta");
+        }
+        catch (ContraseñaIncorrectaException e){
+            //ok;
+        }
+    }
+
+
+
+
+    @Requisitos({"RF1"})
+    @Test
+
+    public void testAccesoPersonalAdmValido(){
         final String NOMB = "Salva";
         final String CONT = "malaga";
         Usuario user1 = new Usuario();
@@ -115,16 +198,47 @@ public class Administrador {
 
         try {
             gestionAccesoPersonal.obtenerPersonal(user1.getUser(), user1.getContrasena());
+            //ok
         }catch (UsuarioNoEncontradoException e) {
-            fail("Error al encontar el administrador");
+            fail("NO Debe lanzar una excepcion ");
         }
         catch (UsuarioIncorrectoException e){
-            fail("Error al encontar el administrador");
+            fail("NO Debe lanzar una excepcion ");
         }
         catch (ContraseñaIncorrectaException e){
-            fail("Error con la password");
+            fail("NO Debe lanzar una excepcion ");
         }
     }
+
+
+
+
+
+    /*
+
+
+    }
+    @Requisitos({"RF6"})
+    @Test
+
+
+    public void testAdmin(){
+        final String NOMB = "Salva";
+        Usuario user_aux = new Usuario();
+        user_aux.setUser(NOMB);
+
+        try{
+           gestionAnadirAutorizados.comprobarAdministrador(user_aux.getUser());
+        }
+        catch (PersonaNoExisteException e){
+            fail("Error al encontrar administrador");
+        }
+        catch (NoEsAdministrativoException e)
+
+    }
+
+ */
+
 
 
 }
