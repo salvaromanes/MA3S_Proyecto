a7d76bc6d862328ffb92811d197c38c855e26711
almubalmu <almubalmu@uma.es>
2022-04-29 00:41:24 +0200

    cambiado administrador a Reportes

--- fintech/fintech-ejb/src/test/java/ma3s/fintech/test/Reportes.java
+++ fintech/fintech-ejb/src/test/java/ma3s/fintech/test/Reportes.java
@@ -9,6 +9,9 @@ import org.junit.Test;
 
 import javax.naming.NamingException;
 
+import java.io.IOException;
+import java.util.Date;
+
 import static java.lang.Long.parseLong;
 import static org.junit.Assert.fail;
 
@@ -16,12 +19,15 @@ public class Reportes {
 
     private static final String UNIDAD_PERSITENCIA_PRUEBAS = "FintechEjbTest";
     private static final String CUENTASAPI = "java:global/classes/GeneracionInfHolanda";
+    private static final String GENERARCSV = "java:global/classes/GenerarCSV";
 
     private ma3s.fintech.ejb.GestionInfHolanda gestionInfHolanda;
+    private ma3s.fintech.ejb.GestionGenerarCSV gestionGenerarCSV;
 
     @Before
     public void setup() throws NamingException {
         gestionInfHolanda = (ma3s.fintech.ejb.GestionInfHolanda) SuiteTest.ctx.lookup(CUENTASAPI);
+        gestionGenerarCSV = (ma3s.fintech.ejb.GestionGenerarCSV) SuiteTest.ctx.lookup(GENERARCSV);
 
         BaseDatos.inicializaBaseDatos(UNIDAD_PERSITENCIA_PRUEBAS);
     }
@@ -150,6 +156,79 @@ public class Reportes {
             fail("No Debe lanzar una excepcion");
         }
 
+    }
+
+
+    @Requisitos({"RF12"})
+    @Test
+
+    public void testGenerarCSV() {
+        Date d = new Date(2022 / 03 / 10);
+
+        Usuario user = new Usuario();
+        user.setUser("Almu");
+        user.setEsAdmin(true);
+
+        try {
+            gestionGenerarCSV.generarCSV(user.getUser(), "Individual", d);
+            //ok
+        } catch (IOException e) {
+            fail("No Debe lanzar una excepcion " + e.getMessage());
+        } catch (PersonaNoExisteException e) {
+            fail("No Debe lanzar una excepcion "  + e.getMessage());
+        }
+
+        try {
+            gestionGenerarCSV.generarCSV(user.getUser(), "Periodico", d);
+            //ok
+        } catch (IOException e) {
+            fail("No Debe lanzar una excepcion "  + e.getMessage());
+        } catch (PersonaNoExisteException e) {
+            fail("No Debe lanzar una excepcion "  + e.getMessage());
+        }
+
+    }
+
+
+    @Requisitos({"RF12"})
+    @Test
+
+    public void testGenerarCSVfalloIndividual() {
+        Date d = new Date(2022 / 03 / 10);
+
+        Usuario user = new Usuario();
+        user.setUser("AlmudenaUser");
+        user.setEsAdmin(false);
+
+        try {
+            gestionGenerarCSV.generarCSV(user.getUser(), "Individual", d);
+            fail("Debe lanzar una excepcion");
+        } catch (IOException e) {
+            fail("Debe lanzar una excepcion PersonaNoExisteException " + e.getMessage());
+        } catch (PersonaNoExisteException e) {
+            //ok
+        }
+    }
+
+
+    @Requisitos({"RF12"})
+    @Test
+
+    public void testGenerarCSVfallo() {
+        Date d = new Date(2022 / 03 / 10);
+
+        Usuario user = new Usuario();
+        user.setUser("AlmudenaUser");
+        user.setEsAdmin(false);
+
+        try {
+            gestionGenerarCSV.generarCSV(user.getUser(), "Periodico", d);
+            fail("Debe lanzar una excepcion");
+        } catch (IOException e) {
+            fail("Debe lanzar una excepcion PersonaNoExisteException " + e.getMessage());
+        } catch (PersonaNoExisteException e) {
+            //ok
+        }
     }
 
 
