2f4c3a0d17f5d17399c990b4249f8cf360fae9be
Álvaro Salas <0619883649@uma.es>
2022-05-18 16:13:31 +0200

    creacion del depositar

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/DepositadaEn.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/DepositadaEn.java
@@ -8,15 +8,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Stateless
-public class CrearDepositadaEn implements GestionDepositadaEn {
+public class DepositadaEn implements GestionDepositadaEn {
     @PersistenceContext(unitName = "FintechEjb")
     private EntityManager em;
 
     @Override
     public void crearDeposito(Pooled pooled, double saldo, Referencia referencia) throws PooledException, SaldoNoSuficiente, ReferenciaException {
 
-        Pooled pooled1 = em.find(Pooled.class, pooled);
-        Referencia referencia1 = em.find(Referencia.class, referencia);
+        Pooled pooled1 = em.find(Pooled.class, pooled.getIban());
+        Referencia referencia1 = em.find(Referencia.class, referencia.getIban());
 
         if(!pooled1.equals(pooled) || pooled1 == null){
             throw new PooledException("La cuenta: " + pooled + " no existe");
@@ -26,15 +26,14 @@ public class CrearDepositadaEn implements GestionDepositadaEn {
             throw new SaldoNoSuficiente("Saldo errorneo");
         }
 
-        if(!referencia1.equals(referencia)){
+        if(!referencia1.equals(referencia) || referencia1 == null){
             throw new ReferenciaException("La referencia no existe");
         }
 
-        DepositadaEn depositadaEn = new DepositadaEn();
-        depositadaEn.setIbanPooled(pooled.getIban());
+        ma3s.fintech.DepositadaEn depositadaEn = new ma3s.fintech.DepositadaEn();
+        depositadaEn.setIbanPooled(pooled);
         depositadaEn.setReferencia(referencia);
         depositadaEn.setSaldo(saldo);
-        depositadaEn.setIbanReferencia(referencia.getIban());
         em.persist(depositadaEn);
 
 
