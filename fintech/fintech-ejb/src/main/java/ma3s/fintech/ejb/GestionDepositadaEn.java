5d88eab28827c500d1f9491b2b54eb8f26d1b109
Álvaro Salas <0619883649@uma.es>
2022-05-18 15:55:56 +0200

    Versión 1 . depositada en

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/GestionDepositadaEn.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/GestionDepositadaEn.java
@@ -1,11 +1,12 @@
 package ma3s.fintech.ejb;
 
 import ma3s.fintech.Pooled;
+import ma3s.fintech.Referencia;
 import ma3s.fintech.Segregada;
-import ma3s.fintech.ejb.excepciones.AccesoException;
+import ma3s.fintech.ejb.excepciones.*;
 
 public interface GestionDepositadaEn {
 
-    void crearDeposito(Pooled pooled, Segregada segregada,double saldo);
+    void crearDeposito(Pooled pooled, Segregada segregada, double saldo, Referencia referencia) throws PooledException, SegregadaException, SaldoNoSuficiente;
 
 }
