5d99876babae5315fb4de128d1afb3f749c0f49e
salvaromanes <salvaromanes@uma.es>
2022-05-18 16:08:56 +0200

    EJB referente a usuario creado junto a su interfaz y sus excepciones.

---
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/OperacionUsuario.java
@@ -0,0 +1,102 @@
+package ma3s.fintech.ejb;
+
+import ma3s.fintech.Cliente;
+import ma3s.fintech.PAutorizada;
+import ma3s.fintech.Usuario;
+import ma3s.fintech.ejb.excepciones.PersonaNoExisteException;
+import ma3s.fintech.ejb.excepciones.UsuarioExistenteException;
+import ma3s.fintech.ejb.excepciones.UsuarioNoEncontradoException;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class OperacionUsuario implements GestionOperacionUsuario {
+    @PersistenceContext(unitName = "FintechEjb")
+    private EntityManager em;
+
+    @Override
+    public void crearUsuario(Usuario usuario) throws UsuarioExistenteException {
+        Usuario user = em.find(Usuario.class, usuario.getUser());
+
+        if(user != null){
+            throw new UsuarioExistenteException("El usuario "+user.getUser()+" ya existe");
+        }
+
+        em.persist(usuario);
+    }
+
+    @Override
+    public void cambiarContraseña(String contrasena, String usuario) throws UsuarioNoEncontradoException {
+        Usuario user = em.find(Usuario.class, usuario);
+
+        if(user == null){
+            throw new UsuarioNoEncontradoException("El usuario "+usuario+" no ha sido encontrado");
+        }
+
+        user.setContrasena(contrasena);
+
+        em.merge(user);
+    }
+
+    @Override
+    public void cambiarEstado(String estado, String usuario) throws UsuarioNoEncontradoException {
+        Usuario user = em.find(Usuario.class, usuario);
+
+        if(user == null){
+            throw new UsuarioNoEncontradoException("El usuario "+usuario+" no ha sido encontrado");
+        }
+
+        user.setEstado(estado);
+
+        em.merge(user);
+    }
+
+    @Override
+    public void asignarAutorizada(PAutorizada pAutorizada, String usuario) throws UsuarioNoEncontradoException, PersonaNoExisteException {
+        Usuario user = em.find(Usuario.class, usuario);
+
+        if(user == null){
+            throw new UsuarioNoEncontradoException("El usuario "+usuario+" no ha sido encontrado");
+        }
+
+        if(pAutorizada == null){
+            throw new PersonaNoExisteException("La persona autorizada "+pAutorizada.getNombre()+" no ha sido encontrada");
+        }
+
+        user.setpAutorizada(pAutorizada);
+
+        em.merge(user);
+    }
+
+    @Override
+    public void asignarCliente(Cliente cliente, String usuario) throws UsuarioNoEncontradoException, PersonaNoExisteException {
+        Usuario user = em.find(Usuario.class, usuario);
+
+        if(user == null){
+            throw new UsuarioNoEncontradoException("El usuario "+usuario+" no ha sido encontrado");
+        }
+
+        if(cliente == null){
+            throw new PersonaNoExisteException("El cliente "+cliente.getIdentificacion()+" no ha sido encontrado");
+        }
+
+        user.setCliente(cliente);
+
+        em.merge(user);
+    }
+
+    @Override
+    public void bloquearUsuario(Long id) throws UsuarioNoEncontradoException {
+        Usuario usuario = em.find(Usuario.class, id);
+
+        if(usuario == null){
+            throw new UsuarioNoEncontradoException("El usuario con id "+id+" no ha sido encontrado");
+        }
+
+        usuario.setEstado("Bloqueado");
+
+        em.merge(usuario);
+    }
+}
