9197920f4e594dc05bfda945a4062ade3e7d6dee
Adrián <0610699992@uma.es>
2022-05-21 19:54:20 +0200

    EJB cuentas getters

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/GestionGetCuentas.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/GestionGetCuentas.java
@@ -1,14 +1,27 @@
 package ma3s.fintech.ejb;
 
 import ma3s.fintech.Cuenta;
+import ma3s.fintech.Pooled;
+import ma3s.fintech.Referencia;
 import ma3s.fintech.Segregada;
 
+import javax.persistence.Query;
 import java.util.List;
 
 public interface GestionGetCuentas {
     public List<Cuenta> getCuentas();
 
     public Segregada getSegregada(String iban);
+
+    public List<Segregada> getSegregadas();
+
+    public Pooled getPooled (String iban);
+
+    public List<Pooled> getPooleds();
+
+    public Referencia getReferencia(String iban);
+
+    public List<Referencia> getReferencias();
 }
 
 
--- fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/GetCuentas.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/GetCuentas.java
@@ -1,6 +1,8 @@
 package ma3s.fintech.ejb;
 
 import ma3s.fintech.Cuenta;
+import ma3s.fintech.Pooled;
+import ma3s.fintech.Referencia;
 import ma3s.fintech.Segregada;
 
 import javax.ejb.Stateless;
@@ -24,6 +26,37 @@ public class GetCuentas implements GestionGetCuentas{
     @Override
     public Segregada getSegregada(String iban) {
         return em.find(Segregada.class,iban);
+    }
+
+    @Override
+    public List<Segregada> getSegregadas(){
+        Query query = em.createQuery("select c from Segregada c");
+        List<Segregada> listaClientes = query.getResultList();
+        return listaClientes;
+    }
+
+    @Override
+    public Pooled getPooled (String iban) {
+        return em.find(Pooled.class,iban);
+    }
+
+    @Override
+    public List<Pooled> getPooleds(){
+        Query query = em.createQuery("select c from Pooled c");
+        List<Pooled> listaClientes = query.getResultList();
+        return listaClientes;
+    }
+
+    @Override
+    public Referencia getReferencia(String iban) {
+        return em.find(Referencia.class,iban);
+    }
+
+    @Override
+    public List<Referencia> getReferencias(){
+        Query query = em.createQuery("select c from Referencia c");
+        List<Referencia> listaClientes = query.getResultList();
+        return listaClientes;
     }
 
 }
