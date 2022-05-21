954b4ac676e4674573379a8a9d630a2bc820edf8
Álvaro Salas <0619883649@uma.es>
2022-05-21 14:30:10 +0200

    rama 3

--- fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/GestionGetCuentas.java
+++ fintech/fintech-ejb/src/main/java/ma3s/fintech/ejb/GestionGetCuentas.java
@@ -1,9 +1,14 @@
 package ma3s.fintech.ejb;
 
 import ma3s.fintech.Cuenta;
+import ma3s.fintech.Segregada;
 
 import java.util.List;
 
 public interface GestionGetCuentas {
     public List<Cuenta> getCuentas();
+
+    public Segregada getSegregada(String iban);
 }
+
+
