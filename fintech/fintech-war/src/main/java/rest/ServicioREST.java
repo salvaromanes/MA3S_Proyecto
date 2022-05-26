c4c3233bc664fdea33978a572c5bb8dfc1f12d04
Álvaro Salas <0619883649@uma.es>
2022-05-26 14:00:26 +0200

    api v1

--- fintech/fintech-war/src/main/java/rest/ServicioREST.java
+++ fintech/fintech-war/src/main/java/rest/ServicioREST.java
@@ -1,4 +1,4 @@
-/*
+
 package rest;
 
 
@@ -17,32 +17,13 @@ import javax.ws.rs.core.Response;
 @Path("/fintech")
 public class ServicioREST {
 
-    @EJB
-    private AccesoAplicacion accesoAplicacion;
-    private GeneracionInfHolanda generacionInfHolanda;
-
-
-    @HeaderParam("User-auth")
-    private String datos;
-
-
+    
     @Path("/api/healthcheck")
     @GET
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getEstado(){
-        Usuario usuario = getUsuario();
-        if(usuario == null){
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
-        try{
-            usuario = accesoAplicacion.refrescarUsuario(usuario);
-            return Response.ok(usuario).build();
-        } catch (CampoVacioException e) {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
-        } catch (AccesoException e) {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
-        }
-
+        String str = "OK";
+        return  Response.ok(str).build();
     }
 
 
@@ -50,41 +31,10 @@ public class ServicioREST {
     @POST
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response infoCliente(Cliente cliente){
-        Usuario usuario = getUsuario();
-        if(usuario == null){
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
+
         return null;
 
     }
 
-
-
-
-
-
-
-
-
-
-    private Usuario getUsuario(){
-        if(datos == null){
-            return null;
-        }
-
-        String[] partes = datos.split(":");
-        if(partes.length != 3){
-            return null;
-        }
-
-        Usuario usuario = new Usuario();
-        usuario.setUser(partes[0]);
-        usuario.setContrasena(partes[1]);
-        usuario.setEsAdmin(Boolean.valueOf((partes[2])));
-
-        return usuario;
-    }
-
-
 }
-*/
+
